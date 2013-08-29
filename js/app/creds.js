14a91cc77f670a2572962ea38b9be0bc5c64539d
Kirubakaran Athmanathan <kirubakaran@gmail.com>
2013-08-29 16:55:45 -0700

    customized app name

--- js/app/creds.js
+++ js/app/creds.js
@@ -1,11 +1,10 @@
-
 var tmp_credentials = {
 	
 	base_login_url: "https://getemailbox.com",
 	base_api_url: "https://api.getemailbox.com",
 	base_listen_url: "https://listen.getemailbox.com",
 
-	app_key: "pkg.dev.auth_login_example",
-	prefix_access_token : 'appname_',
+	app_key: "pkg.dev.publicinbox",
+	prefix_access_token : 'publicinbox_',
 
 };
